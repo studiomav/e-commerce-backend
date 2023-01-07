44b56b2f0cd48a950105cbcd86038ddf986fee0d
Michael Virga <michael@studiomav.com>
2023-01-06 16:27:57 -0800

    created db schema

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,11 @@
-require('dotenv').config();
-
 const Sequelize = require('sequelize');
+require('dotenv').config();
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
+      port: process.env.PORT,
       dialect: 'mysql',
       dialectOptions: {
         decimalNumbers: true,
