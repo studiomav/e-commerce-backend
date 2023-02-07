4cdaaaa91d5f0b9b3a7c43a1ca7890e8c75be154
Michael Virga <michael@studiomav.com>
2023-02-06 21:32:07 -0800

    fixed product deletion

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -115,7 +115,7 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
-  product.destroy({
+  Product.destroy({
     where: {
       id: req.params.id
     }
